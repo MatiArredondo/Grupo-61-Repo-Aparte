5873a877b509a349e22f780ad0dc94265dbf266d
Michael-Rodriguez-Tapia <marodriguez19@uc.cl>
2023-04-20 22:57:50 -0400

    f

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-    belongs_to :user
-    belongs_to :product
+    belongs_to :user, foreign_key: 'id_user'
+    belongs_to :product, foreign_key: 'id_product'
     validates :id_user, presence: true
     validates :id_product, presence: true
     validates :content, presence: true
